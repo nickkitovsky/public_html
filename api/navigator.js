1a0d529bf2687081ee132139cc6bdd94b02f6f06
Kitovsky Nick <nick.kitovsky@gmail.com>
2019-03-10 21:37:21 +0700

    navigator test complete

--- api/navigator.js
+++ api/navigator.js
@@ -1,3 +1,141 @@
+const data = {
+    navigator: {
+        values: {
+            appName: navigator.appName,
+            appCodeName: navigator.appCodeName,
+            appVersion: navigator.appVersion,
+            cookieEnabled: navigator.cookieEnabled,
+            language: navigator.language,
+            languages: navigator.languages,
+            userAgent: navigator.userAgent,
+            platform: navigator.platform,
+            doNotTrack: navigator.doNotTrack,
+            hardwareConcurrency: navigator.hardwareConcurrency,//not in wk but there is a pollyfil
+            maxTouchPoints: navigator.maxTouchPoints,//not in wk, covered by getTouchSupport
+            mediaCapabilities: navigator.mediaCapabilities,//not in wk
+            mediaDevices: null,//async
+            deviceMemory: navigator.deviceMemory,//not in wk
+            connection: navigator.connection,//not in wk
+            onLine: navigator.onLine,
+            keyboard: navigator.keyboard,//not in wk
+            nfc: navigator.nfc,//not in wk
+            permissions: navigator.permissions,// probably can't be used in this case, not in wk
+            presentation: navigator.presentation,// not in wk
+            product: navigator.product,
+            productSub: navigator.productSub,
+            storage: navigator.storage,//not in wk
+            vendor: navigator.vendor,
+            vendorSub: navigator.vendorSub,
+            xr: navigator.xr,//not in wk
+            webkitPersistentStorage: navigator.webkitPersistentStorage,//not in wk
+            webkitTemporaryStorage: navigator.webkitTemporaryStorage,//not in wk
+            getBattery: navigator.getBattery,//not in wk
+            getInstalledRelatedApps: navigator.getInstalledRelatedApps,//not in wk
+            getVRDisplays: navigator.getVRDisplays //not in wk
+        },
+        undefined_values: {
+            "navigator.browserSpecs": navigator.browserSpecs,
+            "navigator.version": navigator.version,
+            "navigator.enumerateDevices": navigator.enumerateDevices,
+            "navigator.webkitGetGamepads": navigator.webkitGetGamepads,
+            "navigator.cpuClass": navigator.cpuClass,
+            "navigator.En": navigator.En,
+            "navigator.browserLanguage": navigator.browserLanguage,
+            "navigator.systemLanguage": navigator.systemLanguage,
+            "navigator.userLanguage": navigator.userLanguage,
+            "navigator.standalone": navigator.standalone,
+            "navigator.jg": navigator.jg,
+            "navigator.pointerEnabled": navigator.pointerEnabled,
+            "navigator.uaf": navigator.uaf,
+            "navigator.ai": navigator.ai,
+            "navigator.msMaxTouchPoints": navigator.msMaxTouchPoints,
+            "navigator.share": navigator.share,
+            "navigator.webdriver": navigator.webdriver,
+            "navigator.msPointerEnabled": navigator.msPointerEnabled,
+            "navigator.taintEnabled": navigator.taintEnabled,
+            "navigator.buildID": navigator.buildID,
+            "navigator.oscpu": navigator.oscpu,
+            "navigator.connection.downlinkMax": navigator.connection.downlinkMax,
+            "navigator.connection.type": navigator.connection.type,
+            "navigator.getBattery.level": navigator.getBattery.level,
+            "navigator.getBattery.charging": navigator.getBattery.charging,
+            "navigator.getBattery.dischargingTime": navigator.getBattery.dischargingTime,
+            "navigator.getBattery.chargingTime": navigator.getBattery.chargingTime
+        },
+        obj: {
+            getGamepads: getGamepads(),//async
+            mimeTypes: getMimeTypes(),
+            plugins: getRegularPlugins(),
+            connection: getConnection()
+        },
+        func: {
+            javaEnabled: navigator.javaEnabled()
+        }
+    },
+    screen: {
+        width: screen.width,
+        height: screen.height,
+        availWidth: screen.availWidth,
+        availHeight: screen.availHeight,
+        colorDepth: screen.colorDepth,
+        pixelDepth: screen.pixelDepth,
+        keepAwake: screen.keepAwake,//not in wk
+        availLeft: screen.availLeft,
+        availTop: screen.availTop,
+        deviceXDPI: screen.deviceXDPI,//not in wk
+    },
+    timezoneOffset: (new Date()).getTimezoneOffset(),
+    dateTimeFormat: Intl.DateTimeFormat().resolvedOptions(),
+    systemColors: systemColors(),
+};
+
+// OBJECTS
+// EMPTY
+//     "navigator.mediaCapabilities": navigator.mediaCapabilities,
+//     "navigator.credentials": navigator.credentials,
+//     "navigator.clipboard": navigator.clipboard,
+//     "navigator.storage": navigator.storage,
+//     "navigator.webkitPersistentStorage": navigator.webkitPersistentStorage,
+//     "navigator.geolocation": navigator.geolocation,
+//     "navigator.webkitTemporaryStorage": navigator.webkitTemporaryStorage,
+//     "navigator.bluetooth": navigator.bluetooth,
+//     "navigator.keyboard": navigator.keyboard,
+//     "navigator.permissions": navigator.permissions,
+//     "navigator.locks": navigator.locks
+// NOT EMPTY
+//     "navigator.serviceWorker": navigator.serviceWorker,
+//     "navigator.presentation": navigator.presentation,
+//     "navigator.mediaDevices": navigator.mediaDevices,
+//     "navigator.usb": navigator.usb,
+// UNDEFINED
+//     "navigator.loadPurpose": navigator.loadPurpose,
+//     "navigator.appMinorVersion": navigator.appMinorVersion,
+//     "navigator.fido": navigator.fido,
+// FUNCTIONS
+//     "navigator.sendBeacon": navigator.sendBeacon,
+//     "navigator.unregisterProtocolHandler": navigator.unregisterProtocolHandler,
+//     "navigator.requestMIDIAccess": navigator.requestMIDIAccess,
+//     "navigator.registerProtocolHandler": navigator.registerProtocolHandler,
+//     "navigator.getGamepads": navigator.getGamepads,
+//     "navigator.webkitGetUserMedia": navigator.webkitGetUserMedia,
+//     "navigator.getBattery": navigator.getBattery,
+//     "navigator.getUserMedia": navigator.getUserMedia,
+//     "navigator.vibrate": navigator.vibrate,
+//     "navigator.requestMediaKeySystemAccess": navigator.requestMediaKeySystemAccess,
+
+
+function getConnection() {
+    con = navigator.connection;
+    return {
+        downlink: con.downlink,
+        effectiveType: con.effectiveType,
+        onchange: con.onchange,
+        rtt: con.rtt,
+        saveData: con.saveData
+    }
+}
+
+
 function getMimeTypes() {
     return Array.from(navigator.mimeTypes).map(mt => {
         return {
@@ -13,25 +151,38 @@ function getMimeTypes() {
         }
     });
 }
-function getCPUBenchmark() {
-    var _speedconstant = 8.9997e-9; //if speed=(c*a)/t, then constant=(s*t)/a and time=(a*c)/s
-    var d = new Date();
-    var amount = 150000000;
-    var estprocessor = 1.7; //average processor speed, in GHZ
-    console.log("JSBenchmark by Aaron Becker, running loop "+amount+" times.     Estimated time (for "+estprocessor+"ghz processor) is "+(Math.round(((_speedconstant*amount)/estprocessor)*100)/100)+"s");
-    for (var i = amount; i>0; i--) {} 
-    var newd = new Date();
-    var accnewd = Number(String(newd.getSeconds())+"."+String(newd.getMilliseconds()));
-    var accd = Number(String(d.getSeconds())+"."+String(d.getMilliseconds())); 
-    var di = accnewd-accd;
-    //console.log(accnewd,accd,di);
-    if (d.getMinutes() != newd.getMinutes()) {
-    di = (60*(newd.getMinutes()-d.getMinutes()))+di}
-    const spd = ((_speedconstant*amount)/di);
-    console.log("Time: "+Math.round(di*1000)/1000+"s, estimated speed: "+Math.round(spd*1000)/1000+"GHZ");
-
-    return Math.round(spd*1000)/1000 + "GHZ";
+function getRegularPlugins() {
+    var plugins = []
+    if (navigator.plugins) {
+        // plugins isn't defined in Node envs.
+        for (var i = 0, l = navigator.plugins.length; i < l; i++) {
+            if (navigator.plugins[i]) { plugins.push(navigator.plugins[i]) }
+        }
+    }
+    // sorting plugins only for those user agents, that we know randomize the plugins
+    // every time we try to enumerate them
+    // if (this.pluginsShouldBeSorted()) {
+    //     plugins = plugins.sort(function (a, b) {
+    //         if (a.name > b.name) { return 1 }
+    //         if (a.name < b.name) { return -1 }
+    //         return 0
+    //     })
+    // }
+    return plugins.map(plugin => {
+        var mimeTypes = Array.from(plugin).map(mt => {
+            return {
+                type: mt.type,
+                sufixes: mt.suffixes
+            };
+        });
+        return {
+            name: plugin.name,
+            description: plugin.description,
+            mimeTypes
+        }
+    }, this)
 }
+
 function systemColors() {
     const colors = ["ActiveCaption", "AppWorkspace", "Background", "ButtonFace", "ButtonHighlight", "ButtonShadow", "ButtonText", "CaptionText", "GrayText", "Highlight", "HighlightText", "InactiveBorder", "InactiveCaption", "InactiveCaptionText", "InfoBackground", "InfoText", "Menu", "MenuText", "Scrollbar", "ThreeDDarkShadow", "ThreeDFace", "ThreeDHighlight", "ThreeDLightShadow", "ThreeDShadow", "Window", "WindowFrame", "WindowText", "ActiveBorder"];
 
@@ -61,128 +212,10 @@ function getGamepads() {
         }
     });
 }
-const data = {
-    // window: {
-    //     devicePixelRatio: window.devicePixelRatio,
-    //     localStorage: hasLocalStorage(),
-    //     sessionStorage: hasSessionStorage(),
-    //     indexedDB: hasIndexedDB(),
-    //     openDatabase: getOpenDatabase(),
-    //     reducedMotion: window.matchMedia("prefers-reduced-motion").matches,
-    // },
-    // // console: {
-    // //     // memory: console.memory// not in wk
-    // // },
-    // document: {
-    //     fonts: getDocumentFonts(),
-    // },
-    // // performance: {
-    // //     memory: performance.memory,// not in wk
-    // // },
-    navigator: {
-        appName: navigator.appName,
-        appCodeName: navigator.appCodeName,
-        appVersion: navigator.appVersion,
-        mimeTypes: getMimeTypes(),
-        cookieEnabled: navigator.cookieEnabled,
-        language: navigator.language,
-        languages: navigator.languages,
-        userAgent: navigator.userAgent,
-        plugins: getRegularPlugins(),
-        platform: navigator.platform,
-        doNotTrack: navigator.doNotTrack,
-        hardwareConcurrency: navigator.hardwareConcurrency,//not in wk but there is a pollyfil
-        maxTouchPoints: navigator.maxTouchPoints,//not in wk, covered by getTouchSupport
-        mediaCapabilities: navigator.mediaCapabilities,//not in wk
-        mediaDevices: null,//async
-        deviceMemory: navigator.deviceMemory,//not in wk
-        connection: navigator.connection,//not in wk
-        onLine: navigator.onLine,
-        keyboard: navigator.keyboard,//not in wk
-        nfc: navigator.nfc,//not in wk
-        permissions: navigator.permissions,// probably can't be used in this case, not in wk
-        presentation: navigator.presentation,// not in wk
-        product: navigator.product,
-        productSub: navigator.productSub,
-        storage: navigator.storage,//not in wk
-        vendor: navigator.vendor,
-        vendorSub: navigator.vendorSub,
-        xr: navigator.xr,//not in wk
-        webkitPersistentStorage: navigator.webkitPersistentStorage,//not in wk
-        webkitTemporaryStorage: navigator.webkitTemporaryStorage,//not in wk
-        getBattery: navigator.getBattery,//not in wk
-        getGamepads: getGamepads(),//async
-        getInstalledRelatedApps: navigator.getInstalledRelatedApps,//not in wk
-        getVRDisplays: navigator.getVRDisplays,//not in wk
-        javaEnabled: navigator.javaEnabled()
-    },
-    screen: {
-        width: screen.width,
-        height: screen.height,
-        availWidth: screen.availWidth,
-        availHeight: screen.availHeight,
-        colorDepth: screen.colorDepth,
-        pixelDepth: screen.pixelDepth,
-        keepAwake: screen.keepAwake,//not in wk
-        availLeft: screen.availLeft,
-        availTop: screen.availTop,
-        deviceXDPI: screen.deviceXDPI,//not in wk
-    },
-    // canvas2d: getCanvasFp(),
-    // webgl: getWebglFp(),
-    // canvasFonts: getCanvasFonts(),
-    // jsFonts: null,//async
-    // canPlayType: getCanPlayTypes(),
-    timezoneOffset: (new Date()).getTimezoneOffset(),
-    // audio: null,//async
-    // webrtc: [],//async
-    dateTimeFormat: Intl.DateTimeFormat().resolvedOptions(),
-    // input: [],//async
-    // touchSupport: getTouchSupport(),
-    // typesSupported: getIsTypeSupported(),
-    // voices: null,//async
-    // dom: createDOMFP(),
-    systemColors: systemColors(),
-    // cpuBenchmark: getCPUBenchmark()
-};
-
-// audioKey(fingerprint => {
-//     data.audio = fingerprint;
-//     console.log('audio done');
-// });
-
 enumerateDevicesKey(devices => {
     data.navigator.mediaDevices = devices;
-    console.log('mediaDevices done');
 });
-
-// jsFontsKey(fonts => {
-//     data.jsFonts = fonts;
-//     console.log('js fonts done');
-// });
-
-// getWebRTCFP(candidate => {
-//     if (candidate) {
-//         data.webrtc.push(candidate);
-//         console.log('webrtc done');
-//     }
-// });
-
-// getInuputFP(i => {
-//     const alreadyExists = data.input.some(input => input.type === i.type);
-
-//     if (!alreadyExists) {
-//         data.input.push(i);
-//     }
-// });
 
 
 const resultString = JSON.stringify(data, null, 2);
-    // document.getElementById("start")
-    // let start=getElementById('start')
-    // let final = document.createElement('div');
-    // final.id = 'finish';
-    // final.innerText=resultString
 document.getElementById("start").innerHTML = resultString;
-console.log(resultString);
-// start.appendChild(final);
